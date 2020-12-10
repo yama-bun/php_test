7ae94d80a47476f7238be1a693c7115a8b03d906
yama-bun <baseball8296@gmail.com>
2020-12-11 06:01:55 +0900

    配列に対して繰り返し処理練習

--- loop.php
+++ loop.php
@@ -1,10 +1,16 @@
 <?php
-$i = 0;
-while (true) {
-    if ($i <= 100) {
-        echo $i . PHP_EOL;
-        $i += 10;
-    } else {
-        break;
-    }
+
+// $numbers = [1, 2, 3, 4, 5];
+
+// foreach ($numbers as $number) {
+//     echo $number * 2 . PHP_EOL;
+// }
+
+$currencies = [
+    'japan'   => 'yen',
+    'us'      => 'dollar',
+    'england' => 'pound',
+];
+foreach ($currencies as $country => $currency) {
+    echo $country . ':' . $currency . PHP_EOL;
 }

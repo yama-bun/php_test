d8f9a73853203973b399b6a29d5291c8f56aa1e0
yama-bun <baseball8296@gmail.com>
2020-12-13 06:28:50 +0900

    phpからデータ追加練習

--- mysql.php
+++ mysql.php
@@ -1,12 +1,34 @@
 <?php
 
-$link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+$link   = mysqli_connect('db', 'book_log', 'pass', 'book_log');
 
 if (!$link) {
     echo 'Error: データーベースに接続出来ませんでした。' . PHP_EOL;
     echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
     exit;
 }
+
 echo 'データベースに接続出来ました。' . PHP_EOL;
+
+$sql = <<<EOT
+INSERT INTO companies (
+    name,
+    establishment_date,
+    founder
+) VALUES (
+    'yosimoto',
+    '2013-01-13',
+    'oosaki syatyo'
+)
+EOT;
+
+$result = mysqli_query($link, $sql);
+if ($result) {
+    echo 'データを追加しました。' . PHP_EOL;
+} else {
+    echo 'Error: データの追加に失敗しました。' . PHP_EOL;
+    echo 'debugging error: ' . mysqli_error($link) . PHP_EOL;
+}
+
 mysqli_close($link);
 echo 'データベースを切断しました。' . PHP_EOL;

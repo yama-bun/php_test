46fb62a514ff283cabcbd3abdd03d6c5cfa924ac
yama-bun <baseball8296@gmail.com>
2020-12-12 21:09:23 +0900

    sqltest

---
+++ initialize_reviews_table.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews
+(
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(255),
+    author VARCHAR
+    (100),
+    status VARCHAR
+    (10),
+    score INTEGER,
+    summary VARCHAR
+    (1000),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) DEFAULT CHARACTER
+    SET=utf8mb4;

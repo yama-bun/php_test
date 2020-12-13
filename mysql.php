866c49f9477d3932e08ce2fb620cdf7c0a87ab65
yama-bun <baseball8296@gmail.com>
2020-12-13 15:00:48 +0900

    phpからsql取得し表示練習

--- mysql.php
+++ mysql.php
@@ -10,25 +10,34 @@ if (!$link) {
 
 echo 'データベースに接続出来ました。' . PHP_EOL;
 
-$sql = <<<EOT
-INSERT INTO companies (
-    name,
-    establishment_date,
-    founder
-) VALUES (
-    'yosimoto',
-    '2013-01-13',
-    'oosaki syatyo'
-)
-EOT;
+$sql = 'SELECT name, founder FROM companies';
+$results = mysqli_query($link, $sql);
 
-$result = mysqli_query($link, $sql);
-if ($result) {
-    echo 'データを追加しました。' . PHP_EOL;
-} else {
-    echo 'Error: データの追加に失敗しました。' . PHP_EOL;
-    echo 'debugging error: ' . mysqli_error($link) . PHP_EOL;
+while ($company = mysqli_fetch_assoc($results)) {
+    echo '会社名：' . $company['name'] . PHP_EOL;
+    echo '代表者名：' . $company['founder'] . PHP_EOL;
 }
+
+mysqli_free_result($results); //メモリ解放。
+// $sql = <<<EOT
+// INSERT INTO companies (
+//     name,
+//     establishment_date,
+//     founder
+// ) VALUES (
+//     'yosimoto',
+//     '2013-01-13',
+//     'oosaki syatyo'
+// )
+// EOT;
+
+// $result = mysqli_query($link, $sql);
+// if ($result) {
+//     echo 'データを追加しました。' . PHP_EOL;
+// } else {
+//     echo 'Error: データの追加に失敗しました。' . PHP_EOL;
+//     echo 'debugging error: ' . mysqli_error($link) . PHP_EOL;
+// }
 
 mysqli_close($link);
 echo 'データベースを切断しました。' . PHP_EOL;
